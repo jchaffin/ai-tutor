a1f232b774df9e6333b0235ae1b2a32ac48d6af9
Jacob Chaffin <jchaffin@ucla.edu>
2025-08-20 10:21:55 -0700

    downgrade node version to support prisma

--- src/app/api/files/[id]/route.ts
+++ src/app/api/files/[id]/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { auth } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
-import { NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
